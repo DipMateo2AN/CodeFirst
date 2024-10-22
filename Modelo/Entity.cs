8e34071cb8505f30d80c32c6794e50653c03d9fa
Mateo Dip <mateo.dip@alumnos.uai.edu.ar>
2024-10-22 20:10:56 -0300

    Agregar archivos de proyecto.

---
+++ Modelo/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public abstract class Entity
+    {
+        private int id {  get; set; }
+    }
+}
